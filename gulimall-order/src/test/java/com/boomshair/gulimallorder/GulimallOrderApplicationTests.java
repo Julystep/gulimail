0bab350cdff5d3898123529b847b3d681242368f
boomshair <957904176@qq.com>
2023-03-20 22:26:04 +0800

    项目初始化

---
+++ gulimall-order/src/test/java/com/boomshair/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boomshair.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
