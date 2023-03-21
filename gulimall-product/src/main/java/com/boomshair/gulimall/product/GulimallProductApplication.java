92515c085def7739625666e839443a1cbda8c906
boomshair <957904176@qq.com>
2023-03-21 22:29:40 +0800

    项目初始化

--- gulimall-product/src/main/java/com/boomshair/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/boomshair/gulimall/product/GulimallProductApplication.java
@@ -1,4 +1,4 @@
-package com.boomshair.gulimall;
+package com.boomshair.gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
